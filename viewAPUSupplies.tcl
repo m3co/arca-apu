da007aa753b76f6feec748540b1e07b390d826b4
Andres Rios <rianby64@gmail.com>
2017-12-10 17:32:21 +0300

    Muestre el keynote y la APU dentro del keynote

--- viewAPUSupplies.tcl
+++ viewAPUSupplies.tcl
@@ -1,5 +1,6 @@
 
 namespace eval viewAPUSupplies {
+  variable frame
 
   proc open'view { space keynote } {
     #viewAPUSupplies::clean'data
@@ -31,6 +32,27 @@ namespace eval viewAPUSupplies {
 
   proc 'do'select { resp } {
     upvar $resp response
-    parray response
+    array set row [deserialize $response(row)]
+    set rowid "$row(keynotes_id),$row(APUSupplies_id),$row(Supplies_id)"
+    variable rows
+    variable frame
+
+    set keynote_frame $frame.[regsub -all {[.]} $row(keynotes_id) "_"]
+    if { [winfo exists $keynote_frame] == 0 } {
+      pack [labelframe $keynote_frame -text $row(keynotes_id) -bg green] \
+        -fill x -expand true
+    }
+    if { $row(APU_id) != "" } {
+      set apu_frame $keynote_frame.apu_$row(APU_id)
+      if { [winfo exists $apu_frame] == 0 } {
+        pack [frame $apu_frame -bg yellow] -side left
+        pack [label $apu_frame.apu_description -text $row(APU_description)] \
+          -side top -fill x -expand true
+        pack [frame $apu_frame.extras] -side left -fill x
+        pack [label $apu_frame.extras.apu_unit -text $row(APU_unit)] -side left
+        pack [label $apu_frame.extras.apu_cost -text $row(APU_cost)] -side left
+        pack [label $apu_frame.extras.apu_qop -text $row(APU_qop)] -side left
+      }
+    }
   }
 }
