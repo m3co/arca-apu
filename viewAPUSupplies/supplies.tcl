161aea1e6b9ce047b3b79d3741f6447760635560
Andres Rios <rianby64@gmail.com>
2018-03-16 06:21:23 +0300

    Evite agregar insumo en combobox inexistente

--- viewAPUSupplies/supplies.tcl
+++ viewAPUSupplies/supplies.tcl
@@ -4,6 +4,9 @@ namespace eval Supplies {
   proc 'do'insert { resp } {
     upvar $resp response
     array set entry [deserialize $response(row)]
+    if { $labelentry::lastEdit(input) == "" } {
+      return
+    }
 
     set response(rows) [list [array get entry]]
     set response(combo) $labelentry::lastEdit(input)
