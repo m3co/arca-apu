fefd1a64aae82ec7fc684de4736a59f609c56ac7
Andres Rios <rianby64@gmail.com>
2018-05-26 13:27:19 +0300

    Trate de llevar un poco mas alla el concepto de tabla dentro de tabla

--- static/preapu.js
+++ static/preapu.js
@@ -31,18 +31,52 @@
   function doselect(row) {
     var storage = doselect.storage;
     var bounceRender = doselect.bounceRender;
-    var found = storage.find(d => d['id'] == row['id']);
+    var found = storage.find(d => d['preAPUId'] == row['preAPUId']);
     if (!found) {
       storage.push(row);
       bounceRender();
+    } else {
+      console.log('que hacer con esto?', row);
     }
   }
 
+  const extrarow = [{
+    update: function(d, i, m) {
+      console.log(d, i, m, 'update');
+    },
+    exit: function(d, i, m) {
+      console.log(d, i, m, 'exit');
+    },
+    enter: function(d, i, m) {
+      var tr = d3.select(this.parentElement)
+        .selectAll(`tr[aauid="${d.AAUId}"]`)
+        .data([d]).enter()
+        .select(function() {
+          return this.insertBefore(
+            document.createElement('tr'), m[i].nextSibling
+          );
+        })
+        .attr('aauid', d.AAUId).append('td')
+          .attr('colspan', 6);
+      var tr1 = tr.append('tr');
+      tr1.append('td').text('ok1');
+      tr1.append('td').text('ok2');
+      tr1.append('td').text('ok3');
+      tr1.append('td').text('ok4');
+
+      var tr1 = tr.append('tr');
+      tr1.append('td').text('ok1');
+      tr1.append('td').text('ok2');
+      tr1.append('td').text('ok3');
+      tr1.append('td').text('ok4');
+    }
+  }];
+
   window.preapu = setupTable({
-    module: 'preAPU',
-    header: header, actions: actions,
+    module: 'viewpreAPUAPUSupplies',
+    header: header, actions: actions, extraRows: extrarow,
     fields: fields, idkey: 'id', validations: validations,
-    defaultRow: defaultRow, filter: { key: 'table', value: 'preAPU' },
-    doselect: doselect
+    defaultRow: defaultRow, doselect: doselect,
+    filter: { key: 'table', value: 'viewpreAPUAPUSupplies' }
   });
 })();
