16c1be2e498c6f5935d0a7b315a5588c265c52e2
Andres Rios <rianby64@gmail.com>
2018-05-27 18:42:53 +0300

    Muestre las APU por fin. Es un copie-y-pegue en realidad

---
+++ static/apu/client.js
@@ -0,0 +1,51 @@
+'use strict';
+((io) => {
+  var client = io();
+
+  client.on('connect', () => {
+    console.log('connection');
+
+    client.emit('data', {
+      query: 'select',
+      module: 'viewAPUSupplies'
+    });
+
+    client.emit('data', {
+      query: 'subscribe',
+      module: 'viewAPUSupplies'
+    });
+
+    client.emit('data', {
+      query: 'subscribe',
+      module: 'Supplies'
+    });
+  });
+
+  client.on('response', (data) => {
+    var query = data.query;
+    if (query == 'search') {
+      if (data.module == 'Supplies') {
+        viewapusupplies.dosearch(data);
+      }
+    }
+    if (data.row) {
+      if (data.module == 'viewAPUSupplies') {
+        if (query == 'select') {
+          viewapusupplies.doselect(data.row);
+        } else if (query == 'update') {
+          viewapusupplies.doupdate(data.row);
+        } else if (query == 'insert') {
+          viewapusupplies.doinsert(data.row);
+        } else if (query == 'delete') {
+          viewapusupplies.dodelete(data.row);
+        } else {
+          console.log('sin procesar viewAPUSupplies', data);
+        }
+      } else {
+        console.log('sin procesar', data.row);
+      }
+    }
+  });
+
+  window.client = client;
+})(io);
