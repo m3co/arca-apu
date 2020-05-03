e73fd04817b58d9b6e9f1da737e01f06c5420946
Anton Novikau <anton.novikov.y@gmail.com>
2020-05-04 00:12:17 +0300

    Add debounce func in util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,17 @@
-export function parseToDotsFormat(value: string) {
-  return `$${value.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')}`;
-}
+export const parseToDotsFormat = (value: string) => `$${value.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')}`;
 
-export function parseToNumber(value: string) {
-  return Number(value.replace(/\D/g, ''));
-}
+export const parseToNumber = (value: string) => Number(value.replace(/\D/g, ''));
+
+export const debounce = (func: (params?: any) => void, delay: number, params?: any) => {
+  let timeout: NodeJS.Timeout;
+
+  const callFunc = () => {
+    timeout = setTimeout(() => func(params), delay);
+  };
+
+  return () => {
+    clearTimeout(timeout);
+
+    callFunc();
+  };
+};
